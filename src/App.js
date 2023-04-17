ef48130386d2f218346af63167bdffb2c662b40a
Marcelo Aybar <marcelo.lzb@gmail.com>
2023-04-17 10:41:40 -0300

    cambios de idioma

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import freeCampLogo from './imagenes/freecodecamp-logo.png';
-import Boton from './componentes/Boton';
-import '../src/styles/Boton.css';
-import Pantalla from '../src/componentes/Pantalla';
-import BotonClear from '../src/componentes/BotonClear';
+import freeCampLogo from './img/freecodecamp-logo.png';
+import Button from './components/Button';
+import '../src/styles/Button.css';
+import Display from './components/Display';
+import ButtonClear from './components/ButtonClear';
 import {useState} from 'react';
 import {evaluate} from 'mathjs';
 
@@ -11,14 +11,14 @@ function App() {
 
   const [ input , setInput] = useState('');
 
-  const agregarInput = val => {
+  const addInput = val => {
     setInput(input + val);
   }
   
 //quiero que no se pueda ingresar dos veces una expresion -+*/ osea una vez q escucho el evento se anulen esos botones(crear una constante 'anular expresion' darle un valor que sea - o + o / etc) 
 //quiero pasar const resuldao a la otra manera de escribir if else
 
-  const calcularResultado = () =>{ 
+  const calculateResult = () =>{ 
      if (input){
       setInput(evaluate(input));
     } else{
@@ -30,40 +30,40 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='logo-contenedor'>
+      <div className='logo-conteiner'>
         <img src={ freeCampLogo} className='logo' alt='logo free'  />
 
       </div>
-      <div className='contenedor-calculadora'>
-          <Pantalla  input={input} />
-          <div className='fila'>
-            <Boton manejarClick={agregarInput}>1</Boton>
-            <Boton manejarClick={agregarInput}>2</Boton>
-            <Boton manejarClick={agregarInput}>3</Boton>
-            <Boton manejarClick={agregarInput}>+</Boton>
+      <div className='conteiner-calculator'>
+          <Display  input={input} />
+          <div className='row'>
+            <Button handleClick={addInput}>1</Button>
+            <Button handleClick={addInput}>2</Button>
+            <Button handleClick={addInput}>3</Button>
+            <Button handleClick={addInput}>+</Button>
           </div>
-          <div className='fila'>
-            <Boton manejarClick={agregarInput}>4</Boton>
-            <Boton manejarClick={agregarInput}>5</Boton>
-            <Boton manejarClick={agregarInput}>6</Boton>
-            <Boton manejarClick={agregarInput}>-</Boton>
+          <div className='row'>
+            <Button handleClick={addInput}>4</Button>
+            <Button handleClick={addInput}>5</Button>
+            <Button handleClick={addInput}>6</Button>
+            <Button handleClick={addInput}>-</Button>
           </div>
-          <div className='fila'>
-            <Boton manejarClick={agregarInput}>7</Boton>
-            <Boton manejarClick={agregarInput}>8</Boton>
-            <Boton manejarClick={agregarInput}>9</Boton>
-            <Boton manejarClick={agregarInput}>*</Boton>
+          <div className='row'>
+            <Button handleClick={addInput}>7</Button>
+            <Button handleClick={addInput}>8</Button>
+            <Button handleClick={addInput}>9</Button>
+            <Button handleClick={addInput}>*</Button>
           </div>
-          <div className='fila'>
-            <Boton manejarClick={calcularResultado}>=</Boton>
-            <Boton manejarClick={agregarInput}>0</Boton>
-            <Boton manejarClick={agregarInput}>1</Boton>
-            <Boton manejarClick={agregarInput}>/</Boton>
+          <div className='row'>
+            <Button handleClick={calculateResult}>=</Button>
+            <Button handleClick={addInput}>0</Button>
+            <Button handleClick={addInput}>.</Button>
+            <Button handleClick={addInput}>/</Button>
           </div>
-          <div className='fila'>
-            <BotonClear manejarClear={() => setInput('') }  >
+          <div className='row'>
+            <ButtonClear handleClear={() => setInput('') }  >
               Clear
-            </BotonClear>
+            </ButtonClear>
           </div>
       </div>
     </div>
